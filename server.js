a133eaa90fc027c2331f24bd2e5e7820bd442e4c
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-04-13 20:05:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const logger = require("morgan");
 
 const PORT = 3000;
 
