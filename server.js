dd390c22049a8a5cd07e39c4a188b8578c050a30
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-05-27 22:00:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const logger = require("morgan");
-
 const PORT = process.env.PORT || 3000
-
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
